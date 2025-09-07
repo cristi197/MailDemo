b680b237ec2b970c39f439bf6d2c0e61826cf834
Cristi <cristian.teodorescu97@yahoo.ro>
2025-09-07 14:02:59 +0300

    Add demo for sending email using MimeKit and MailKit with local SMTP

---
+++ MailDemoProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
+
+var message = new MimeMessage();
+
+var from = new MailboxAddress("Cristian", "cristian.teodorescu97@gmail.com");
+message.From.Add(from);
+var to = new MailboxAddress("Alexandru", "cristian.teodorescu97@yahoo.com");
+message.To.Add(to);
+message.Subject = "Let's test MimeKit for a simple email demo.";
+message.Body = new TextPart(TextFormat.Plain) 
+{
+    Text = """
+
+    Hello Alexandru,
+
+    Let's test the functionality of MimeKit library in a simple email!
+
+    Thanks,
+    Cristi
+    """
+};
+
+using var smtp = new SmtpClient();
+await smtp.ConnectAsync("localhost", 1025);
+await smtp.SendAsync(message);
+await smtp.DisconnectAsync(true);
+Console.WriteLine("Mail sent!");
